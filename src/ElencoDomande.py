e3d984b0bdacf6bf1ad41833347dea6db462e749
IvoryBerry <elisaluzi19@gmail.com>
2022-05-31 09:06:37 +0200

    bo

--- src/ElencoDomande.py
+++ src/ElencoDomande.py
@@ -2,24 +2,13 @@ import csv
 
 def load():
     file = open( "domande.csv" , "r")
-    lettore = csv.DictReader(file,delimiter = "/")
-    domandeTot = {}
+    lettore = csv.DictReader(file,delimiter = " ")
+    domandeCanti=[]
     for riga in lettore:
-        if riga["Argomento"] not in domandeTot:
-            domandeTot[riga["Argomento"]] = []
-        domandeTot[riga["Argomento"]].appen((riga["Argomento"], riga["Domanda"], riga["RispostaA"], riga["RispostaB"], riga["RispostsC"], riga["RispostaEsatta"]))
+        if riga["Argomento"] == "Canti":
+            print(riga)
     file.close()
     
-    return domandeTot
+    return
 
-
-class elencodomande:
-    def __init__(self):
-        self.listaDomande = load()
-        return
-
-if __name__ == "__main__":
-    e = elencodomande()
-    print(e.listaDomande)
-    for n in e.listaDomande:
-        print(n)
+load()
